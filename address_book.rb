d3d8991d29c6bb2bb8bc369763849de339895393
chris_hopkins_2445 <cjhopkins1986@gmail.com>
2018-03-15 16:17:42 +0000

    added the view controller

--- address_book.rb
+++ address_book.rb
@@ -25,6 +25,41 @@ class AddressBook
   end
 end
 
+class View
+  attr_reader :contacts
+  def initialize(addressbook)
+    @addressbook = addressbook
+  end
+  def mainMenu
+    run = true
+    while run
+      puts '1)List All Contacts 2)Search for Contact 3)Add new Contact 4)Delete Contact 5)Quit'
+      choice = gets.strip.chomp.to_i
+      case choice
+      when 1
+        puts @addressbook.printContacts
+      when 2
+        puts 'please enter the name of the contact you wish to search for'
+        nm = gets.strip.chomp.to_s
+        puts @addressbook.printContactsByName(nm)
+      when 3
+        puts 'please enter a name for the new contact..'
+        name = gets.strip.chomp.to_s
+        puts 'please enter a telephone number'
+        tel = gets.strip.chomp.to_s
+        @addressbook.addNewContact(name,tel)
+      when 4
+        @addressbook.listContactsByIndex
+        puts 'which contact would you like to delete?'
+        i = gets.strip.chomp.to_i
+        @addressbook.delContactByIndex(i)
+      when 5
+        run = false
+      end
+    end
+  end
+end
+
 class Contact
   attr_accessor :name,:tel
   def initialize(name,tel)
@@ -41,4 +76,5 @@ ben = Contact.new('Ben',"07809671734")
 dan = Contact.new('Dan',"07809671734")
 amy = Contact.new('Amy',"07809671734")
 book = AddressBook.new([chris,ben,dan,amy])
-book.delContactByIndex(2)
+view = View.new(book)
+view.mainMenu
